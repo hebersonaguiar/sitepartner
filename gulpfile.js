8e4dac319b181a8cf6363b6d773fbefe411d7dbe
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-02-22 23:50:22 -0300

    adicionando plugin watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
 
 
 gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'))
+	return sass('sass/**/*.sass').pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass'])
 });
 
 gulp.task('default', function(){
