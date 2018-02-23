9cc4b69974f677147d726aa41c41f667d7725fc4
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-02-22 23:53:37 -0300

    refatorando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var watch = require('gulp-watch');
 
-
+//task for sass
 gulp.task('sass', function(){
 	return sass('sass/**/*.sass').pipe(gulp.dest('css'))
 });
 
+//task for watch
 gulp.task('watch', function(){
 	gulp.watch('sass/**/*.sass', ['sass'])
 });
 
-gulp.task('default', function(){
-	
-});
+//task default gulp
+gulp.task('default', ['sass','watch']);
