da1cc98cdd65932a711641a00e3eb3cff71c7209
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-02-22 22:36:16 -0300

    Compilando arquivos .sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
 
 gulp.task('sass', function(){
-	
+	return sass('sass/*.sass').pipe(gulp.dest('css'))
 });
 
 gulp.task('default', function(){
