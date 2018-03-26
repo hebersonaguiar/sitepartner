49e98bf5d29f4d9da41ad02961cff6a2c718805d
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-03-25 23:21:56 -0300

    edit contato php

--- contato.php
+++ contato.php
@@ -1,6 +1,7 @@
 <?php 
 	$style = 'contato';
 	$chamada = '<h1>Contato</h1>';
-	include('header_part.php');
+	// include('');
+	get_header('header_part.php');
 	include('includes/pages/contato.php');
 	include('footer.php');
