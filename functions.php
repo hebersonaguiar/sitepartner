d12a5f4df3a7a8e7378362641ab0fc3da6fe3a6f
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-03-27 22:44:59 -0300

    criando funcao para o menu

--- functions.php
+++ functions.php
@@ -1,2 +1,13 @@
 <?php
+	//ativando suporte a imagem destacada
 	add_theme_support( 'post-thumbnails' );	
+
+	//ativando menus dinamicos
+	function register_menu(){
+		register_nav_menus(
+			array(
+					'header-menu' => __('Menu Topo')
+			)
+		);
+	}
+	add_action( 'init', 'register_menu' );
