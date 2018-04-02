76e627b4f9d99b09a74812df2249c2d77be5337c
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-01 21:30:45 -0300

    editando procedimentos page

--- includes/pages/procedimentos.php
+++ includes/pages/procedimentos.php
@@ -2,7 +2,7 @@
 	<section class="sec-procedimentos">
 		<div class="thumb-um">
 			<?php $queried_post = get_post(92); ?>
-				<?php if(get_the_post_thumbnail()) : ?>
+				<?php if(get_the_post_thumbnail(92)) : ?>
 					<img src="<?php the_post_thumbnail_url(); ?>" alt="Densitometria Óssea">
 				<?php else : ?>
 
