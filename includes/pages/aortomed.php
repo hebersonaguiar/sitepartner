6655c0e9d41cc9f0c81342d48ac4b9b6b6944683
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-02 22:11:18 -0300

    editando contato em home page

--- includes/pages/aortomed.php
+++ includes/pages/aortomed.php
@@ -1,61 +1,69 @@
 <div class="container">
-	<section class="sec-ortomed">
-		<div class="aortomed">
-			<?php if(have_posts()): ?>
-				<?php while(have_posts()): the_post(); ?>
-					<p><?php the_content(); ?></p>
-				<?php endwhile; ?>
-			<?php endif; ?>
-			<!-- <p>A Ortomed atua em Vitória da Conquista há mais de 30 anos em diversas especialidades e tem como objetivo prestar com excelência e qualidade serviços de saúde, sempre oferecendo um acompanhamento individualizado e completo através de procedimentos seguros e priorizando sempre uma análise minuciosa de cada caso. O nosso corpo clinico conta com profissionais experientes e altamente qualificados, permanentemente atualizados com os avanços da medicina.</p>
-			<p>Atualmente, a Clínica disponibiliza, por meio de equipamentos modernos e de uma equipe de profissionais comprometida com a saúde e bem-estar dos pacientes, serviços como: Densitometria Óssea, Eletroneuromiografia, Fisioterapia e reabilitação, Pronto Atendimento, Radiologia _ Incluindo Raio X Panorâmico de Coluna e Membros Inferiores, Ultrassonografia, Reeducação Postural Global ( R.P.G) e ITC.</p>
-			<p>A Ortomed possui também especialidades nas áreas de Acupuntura, Angiologia, Cirurgia do Joelho, Cirurgia e Traumatologia Bucomaxilofacial e Implantodontia, Clinica Médica, Dermatologia, Medicina do Trabalho, Nutrição, Neurologia e Neurofisiologia, Ortopedia e Traumatologia, Ortopedia Pediátrica, Psicologia, Psiquiatria, Reumatologia.</p> -->
-		</div>
-		<div class="cnes">
-			<span>CNES 2488981</span>
-		</div>
-		<div class="cnpj">	
-			<span>CNPJ: 13.768.3200001-38</span>
-		</div>
-		<div class="itc-vet">
-			<div class="itc-vet-align">
-				<a href="#">
-					<img src="<?php bloginfo('template_url'); ?>/images/itc-vet.png" alt="Itc Vertebral">
-				</a>
-			</div>
-		</div>
-	</section>
+   <section class="sec-ortomed">
+      <div class="aortomed">
+         <?php if(have_posts()): ?>
+         <?php while(have_posts()): the_post(); ?>
+         <p><?php the_content(); ?></p>
+         <?php endwhile; ?>
+         <?php endif; ?>
+         <!-- <p>A Ortomed atua em Vitória da Conquista há mais de 30 anos em diversas especialidades e tem como objetivo prestar com excelência e qualidade serviços de saúde, sempre oferecendo um acompanhamento individualizado e completo através de procedimentos seguros e priorizando sempre uma análise minuciosa de cada caso. O nosso corpo clinico conta com profissionais experientes e altamente qualificados, permanentemente atualizados com os avanços da medicina.</p>
+            <p>Atualmente, a Clínica disponibiliza, por meio de equipamentos modernos e de uma equipe de profissionais comprometida com a saúde e bem-estar dos pacientes, serviços como: Densitometria Óssea, Eletroneuromiografia, Fisioterapia e reabilitação, Pronto Atendimento, Radiologia _ Incluindo Raio X Panorâmico de Coluna e Membros Inferiores, Ultrassonografia, Reeducação Postural Global ( R.P.G) e ITC.</p>
+            <p>A Ortomed possui também especialidades nas áreas de Acupuntura, Angiologia, Cirurgia do Joelho, Cirurgia e Traumatologia Bucomaxilofacial e Implantodontia, Clinica Médica, Dermatologia, Medicina do Trabalho, Nutrição, Neurologia e Neurofisiologia, Ortopedia e Traumatologia, Ortopedia Pediátrica, Psicologia, Psiquiatria, Reumatologia.</p> -->
+      </div>
+      <div class="cnes">
+         <span>CNES 2488981</span>
+      </div>
+      <div class="cnpj">	
+         <span>CNPJ: 13.768.3200001-38</span>
+      </div>
+      <div class="itc-vet">
+         <div class="itc-vet-align">
+            <a href="#">
+            <img src="<?php bloginfo('template_url'); ?>/images/itc-vet.png" alt="Itc Vertebral">
+            </a>
+         </div>
+      </div>
+   </section>
 </div>
 </main>	
 <main class="second-main">
-<div class="container">
-	<section class="sec-dep">
-		<div class="dep-nome">
-			<div class="slide-dep">
-				<h1>Depoimentos</h1>
-			</div>
-			<div class="barra"></div>				
-		</div>
-		<div class="depoimentos">
-			<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-			<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-		</div>
-	</section>
-</div>
+   <div class="container">
+      <section class="sec-dep">
+         <div class="dep-nome">
+            <div class="slide-dep">
+               <h1>Depoimentos</h1>
+            </div>
+            <div class="barra"></div>
+         </div>
+         <div class="depoimentos">
+            <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+         </div>
+      </section>
+   </div>
 </main>
 <main class="third-main">
 <section class="sec-medias">
-	<div class="container">
-		<div class="iframe-face">
-			<span>facebook</span>
-			<div class="clear"></div>
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-			<div class="iframe-insta">
-				<span>instagram</span>
-				<div class="clear"></div>
-				<blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p></div></blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="iframe-face">
+         <span>facebook</span>
+         <div class="clear"></div>
+         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+         <div class="iframe-insta">
+            <span>instagram</span>
+            <div class="clear"></div>
+            <blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;">
+               <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
+                  <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div>
+               </div>
+               <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p>
+         </div>
+         </blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
+      </div>
+   </div>
+   </div>
 </section>
