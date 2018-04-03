cbe5e19179127d28ff398a3f6a5de4f88432f536
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-02 23:24:56 -0300

    formatando contato page

--- includes/pages/contato.php
+++ includes/pages/contato.php
@@ -1,53 +1,58 @@
 <div class="container">
-	<section class="sec-contato">
-		<div class="form-cont">
-			<span>Converse com a gente!</span>
-			<?php 
-				the_post();
-				the_content();
-			?>
-		</div>
-		<div class="horario">
-			<h1>Horarío de funcionamento</h1>
-			<ul>
-				<li>Segunda a Quinta: 07:00 às 19:00</li>
-				<li>Sexta: 07:00 às 18:00</li>
-				<li>Sábado: 07:00 às 12:00</li>
-			</ul>
-		</div>
-		<div class="localizacao">
-			<h1>Localização</h1>
-			<span>Avenida Bartolomeu de Gusmão, 726, Vitória da Conquista, Bahia, Cep: 45020-107</span>
-		</div>
-		<div class="telefone-cont">
-			<h1>Telefone para contato</h1>
-			<span>(77) 2101-0500</span>
-		</div>
-		<div class="map">
-
-			<iframe width="570" height="400" src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;q=Ortomed%20Policl%C3%ADnica%20-%20Avenida%20Bartolomeu%20de%20Gusm%C3%A3o%20-%20Jurema%2C%20Vit%C3%B3ria%20da%20Conquista%20-%20BA%2C%20Brasil+(Ortomed%20Policl%C3%ADnica)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-		</div>
-	</section>
+   <section class="sec-contato">
+      <div class="form-cont">
+         <span>Converse com a gente!</span>
+         <?php 
+            the_post();
+            the_content();
+            ?>
+      </div>
+      <div class="horario">
+         <h1>Horarío de funcionamento</h1>
+         <ul>
+            <li>Segunda a Quinta: 07:00 às 19:00</li>
+            <li>Sexta: 07:00 às 18:00</li>
+            <li>Sábado: 07:00 às 12:00</li>
+         </ul>
+      </div>
+      <div class="localizacao">
+         <h1>Localização</h1>
+         <span>Avenida Bartolomeu de Gusmão, 726, Vitória da Conquista, Bahia, Cep: 45020-107</span>
+      </div>
+      <div class="telefone-cont">
+         <h1>Telefone para contato</h1>
+         <span>(77) 2101-0500</span>
+      </div>
+      <div class="map">
+         <iframe width="570" height="400" src="https://maps.google.com/maps?width=720&amp;height=600&amp;hl=en&amp;q=Ortomed%20Policl%C3%ADnica%20-%20Avenida%20Bartolomeu%20de%20Gusm%C3%A3o%20-%20Jurema%2C%20Vit%C3%B3ria%20da%20Conquista%20-%20BA%2C%20Brasil+(Ortomed%20Policl%C3%ADnica)&amp;ie=UTF8&amp;t=&amp;z=17&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      </div>
+   </section>
 </div>
 </main>	
 <main class="second-main">
-<div class="container">
-	<section class="sec-dep">
-	</section>
-</div>
+   <div class="container">
+      <section class="sec-dep">
+      </section>
+   </div>
 </main>
 <main class="third-main">
 <section class="sec-medias">
-	<div class="container">
-		<div class="iframe-face">
-			<span>facebook</span>
-			<div class="clear"></div>
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-			<div class="iframe-insta">
-				<span>instagram</span>
-				<div class="clear"></div>
-				<blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p></div></blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="iframe-face">
+         <span>facebook</span>
+         <div class="clear"></div>
+         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fortomedpoliclinica%2F&tabs=timeline&width=320&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="320" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+         <div class="iframe-insta">
+            <span>instagram</span>
+            <div class="clear"></div>
+            <blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;">
+               <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
+                  <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div>
+               </div>
+               <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p>
+         </div>
+         </blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
+      </div>
+   </div>
+   </div>
 </section>
