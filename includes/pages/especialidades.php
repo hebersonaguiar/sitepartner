520e5d8bae8944b77d685081067e3c68af74d4f7
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-03 21:38:12 -0300

    editando pagina especialidades

--- includes/pages/especialidades.php
+++ includes/pages/especialidades.php
@@ -1,67 +1,102 @@
 <div class="container">
    <section class="sec-especialidades">
       <div class="especialidades">
-         <!-- 			<?php query_posts( 'post_type=especialidades&post_per_page=-1' ); ?>
+
+<!--        <?php query_posts( 'post_type=especialidades&post_per_page=-1' ); ?>
+
             <?php if(have_posts()): ?> 
-            
-            	<?php while(have_posts()): the_post(); ?> -->
-         <!-- 					<table>
-            <tr>
-            <td><i class="fas fa-user-md"></i>   <a href="#"><?php the_title() ?></a></td>
-            </tr>
-            </table> -->
-         <!-- 					<?php endwhile; ?>
-            <?php else : ?> -->
-         <!-- 					<table>
-            <tr>
-            <td><i class="fas fa-user-md"></i>   <a href="#">Sem Especialidades Cadastradas...</a></td>
-            </tr>
-            </table> -->
-         <!-- 					<?php endif; ?>
-            <?php wp_reset_query(); ?> -->
+
+               <?php while(have_posts()): the_post(); ?> -->
+<!--              <table>
+                  <tr>
+                     <td><i class="fas fa-user-md"></i>   <a href="#"><?php the_title() ?></a></td>
+                  </tr>
+               </table> -->
+<!--              <?php endwhile; ?>
+
+               <?php else : ?> -->
+<!--              <table>
+                 <tr>
+                  <td><i class="fas fa-user-md"></i>   <a href="#">Sem Especialidades Cadastradas...</a></td>
+                 </tr>
+               </table> -->
+<!--              <?php endif; ?>
+
+         <?php wp_reset_query(); ?> -->
+
+
          <table>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Acupuntura</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Cirurgia Vascular</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Clínica Médica</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Dermatologia</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Fisioterapia</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Medicina do Trabalho</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Neurologia</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia e Traumatologia</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia Pediátrica</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia de Joelho</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Psicologia</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Psiquiatria</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Pilates</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Radiologia</a></td>
-            </tr>
-            <tr>
-               <td><i class="fas fa-user-md"></i>   <a href="#">Reumatologia</a></td>
-               <td><i class="fas fa-user-md"></i>   <a href="#">RPG</a></td>
-            </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Acupuntura</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Cirurgia Vascular</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Clínica Médica</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Dermatologia</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Fisioterapia</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Medicina do Trabalho</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Neurologia</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia e Traumatologia</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia Pediátrica</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Ortopedia de Joelho</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Psicologia</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Psiquiatria</a></td>
+           </tr>
+           <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Pilates</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Radiologia</a></td>
+           </tr>
+              <tr>
+             <td><i class="fas fa-user-md"></i>   <a href="#">Reumatologia</a></td>
+             <td><i class="fas fa-user-md"></i>   <a href="#">RPG</a></td>
+           </tr>
          </table>
       </div>
+<!--           <div class="aortomed">
+         <p>A Ortomed atua em Vitoria da Conquista há mais de 20 anos e tem como objetivo prestar serviços, oferecendo um acompanhamento completo da saúde dos pacientes, através de procedimentos seguros e priorizando sempre uma análise minuciosa de cada caso. Atualmente, a Clínica disponibiliza, por meio de equipamentos modernos e de uma equipe de profissionais comprometida com a saúde e bem-estar dos pacientes, serviços como: Densitometria Óssea, Fisioterapia e reabilitação, Pronto Atendimento, Radiologia, Ultrassonografia, Reeducação Postural Global ( R.P.G) e ITC.</p>
+         <p>A Ortomed possui também especialidades nas áreas de Acupuntura, Angiologia, Cirurgia do Joelho, Cirurgia e Traumatologia Bucomaxilofacial e Implantodontia, Medicina do Trabalho, Neurologia, Psicologia, Ortopedia e Traumatologia, Ortopedia Pediátrica.</p>
+      </div>
+      <div class="cnes">
+         <span>CNES 2488981</span>
+      </div>
+      <div class="cnpj">   
+         <span>CNPJ: 13.768.3200001-38</span>
+      </div>
+      <div class="itc-vet">
+         <div class="itc-vet-align">
+            <a href="#">
+               <img src="images/itc-vet.png" alt="Itc Vertebral">
+            </a>
+         </div>
+      </div> -->
    </section>
 </div>
-</main>	
+</main>  
 <main class="second-main">
-   <div class="container">
-      <section class="sec-dep">
-      </section>
-   </div>
+<div class="container">
+   <section class="sec-dep">
+<!--           <div class="dep-nome">
+         <div class="slide-dep">
+            <h1>Depoimentos</h1>
+         </div>
+         <div class="barra"></div>           
+      </div>
+      <div class="depoimentos">
+         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+         proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+         <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+         proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div> -->
+   </section>
+</div>
 </main>
 <main class="third-main">
 <section class="sec-medias">
@@ -73,14 +108,8 @@
          <div class="iframe-insta">
             <span>instagram</span>
             <div class="clear"></div>
-            <blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;">
-               <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;">
-                  <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div>
-               </div>
-               <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p>
+            <blockquote width="320" height="400" class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/BgQ-Rqfhtyl/" data-instgrm-version="8" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width: 320px; height:400px;><div style="padding:8px;"> <div style=" background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;"> <div style=" background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;"></div></div><p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;"><a href="https://www.instagram.com/p/BgQ-Rqfhtyl/" style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none;" target="_blank">Uma publicação compartilhada por Ortomed Policlínica (@ortomed)</a> em <time style=" font-family:Arial,sans-serif; font-size:14px; line-height:17px;" datetime="2018-03-13T13:32:53+00:00">13 de Mar, 2018 às 6:32 PDT</time></p></div></blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
          </div>
-         </blockquote> <script async defer src="//www.instagram.com/embed.js"></script>
       </div>
-   </div>
    </div>
 </section>
