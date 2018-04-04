07a498bc109ba06fa17ee13b1d8fb593c1062de3
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-03 21:48:34 -0300

    editando functions e especialidades

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+	//ativando suporte a imagem destacada
+	add_theme_support( 'post-thumbnails' );	
+
+	//ativando menus dinamicos
+	function register_menu(){
+		register_nav_menus(
+			array(
+					'header-menu' => __('Menu Topo')
+			)
+		);
+	}
+	add_action( 'init', 'register_menu' );
+
+	//posts types
+	// function posts_types(){
+	// 	//especialidades
+	// 	register_post_type('especialidades',
+	// 		array(
+	// 			'labels' 			=> array(
+	// 				'name' 			=> __('Especialidades'),
+	// 				'singular_name' => __('Especialidade')
+	// 				),
+	// 			'public' 		=> true,
+	// 			'has_archive' 	=> true,
+	// 			'menu_icon'		=> 'dashicons-heart',
+	// 			'supports'		=> array('title', 'editor', 'thumbnail', 'page-attributes'),
+	// 		)
+	// 	);
+		// register_post_type('procedimentos',
+		// 		array(
+		// 			'labels' 			=> array(
+		// 				'name' 			=> __('Procedimentos'),
+		// 				'singular_name' => __('Procedimento')
+		// 				),
+		// 			'public' 		=> true,
+		// 			'has_archive' 	=> true,
+		// 			'menu_icon'		=> 'dashicons-heart',
+		// 			'supports'		=> array('title', 'editor', 'thumbnail', 'page-attributes'),
+		// 	)
+		// );
+	// }
+	// add_action( 'init', 'posts_types' );
