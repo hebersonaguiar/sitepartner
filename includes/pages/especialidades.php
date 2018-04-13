c22418e85760d3da4bde658043ed812be31f7f11
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-12 22:43:04 -0300

    editando procedimentos

--- includes/pages/especialidades.php
+++ includes/pages/especialidades.php
@@ -6,7 +6,7 @@
 				<?php
 					global $more;
 					$more = 0;
-					query_posts('cat=5');
+					query_posts('cat=5&post_type=especialidades');
 					if(have_posts()) : while(have_posts()) : the_post();
 				?>
 			   		 <td><i class="fas fa-user-md"></i> <a href="#"><?php the_title(); ?></a></td>
