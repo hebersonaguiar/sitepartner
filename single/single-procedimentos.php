c1441ec7755a0075d2c043ee529173d2b675264d
Heberson Aguiar <hebersonaguiar@gmail.com>
2018-04-16 22:23:49 -0300

    criando post template

---
+++ single/single-procedimentos.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+	   template name: Post Template
+	   template post type: post, page, product
+	 */
+	$style = 'aortomed';
+	include('../header-in.php');
+	include('../includes/pages/posttemplate.php');
+	include('../footer.php');
